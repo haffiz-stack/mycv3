badf663856e26ae3570fd4c526e81b0a116cbd76
haffiz <haffiz.samad@i-serve.com.my>
2021-03-23 14:41:30 +0800

    alter welcome
    alter welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel -MyCV3
                 </div>
 
                 <div class="links">
