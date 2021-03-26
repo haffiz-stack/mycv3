53f8e4b95f7b25513e738bbb2bfc35a0c7b31eae
haffiz <haffiz.samad@i-serve.com.my>
2021-03-26 14:10:46 +0800

    job svchedule
    job svchedule

---
+++ app/Console/Commands/MinuteUpdate.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MinuteUpdate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'minute:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        \Mail::to('haffiz_ito@yahoo.com')->send(new \App\Mail\testMail($details));
+        $this->info('email sent every minute');
+        return 0;
+    }
+}
