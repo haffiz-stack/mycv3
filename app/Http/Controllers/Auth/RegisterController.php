184ebfd56f8ae08d7e105599ff30fca0fbca0c08
haffiz <haffiz.samad@i-serve.com.my>
2021-03-26 09:38:06 +0800

    register email fix
    register email fix

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,7 +64,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+     $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
@@ -76,6 +76,8 @@ class RegisterController extends Controller
         ];
 
         \Mail::to('haffiz_ito@yahoo.com')->send(new \App\Mail\testMail($details));
-        dd('s');
+
+        return $user;
+        // dd('s');
     }
 }
